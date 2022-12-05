f6ca5885ec3cb763ba322253c3cf4b8b5ccb4f9a
0marA <theengineermachine@gmail.com>
2022-12-05 17:24:03 -0600

    Update port

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,7 @@ import multer from "multer";
 import path from "path";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5738;
 
 
 const storage = multer.memoryStorage();
