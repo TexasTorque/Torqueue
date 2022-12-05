88dd9833bb7f50f5ddd4b81c09044d27c36af4ea
0marA <theengineermachine@gmail.com>
2022-12-05 17:06:57 -0600

    Remove unused function

--- client/src/components/ManagePopup.tsx
+++ client/src/components/ManagePopup.tsx
@@ -102,35 +102,35 @@ export default function ManagePopup({
         const data = byteData.data; // assume you have the data here
         console.log(data);
         //const arrayBuffer = base64ToArrayBuffer(data);
-        createAndDownloadBlobFile(data, "testName");
+        //createAndDownloadBlobFile(data, "testName");
     };
 
-    function base64ToArrayBuffer(base64: string) {
-        const binaryString = window.atob(base64); // Comment this if not using base64
-        const bytes = new Uint8Array(binaryString.length);
-        return bytes.map((byte, i) => binaryString.charCodeAt(i));
-    }
-
-    function createAndDownloadBlobFile(
-        body: any,
-        filename: any,
-        extension = "pdf"
-    ) {
-        const blob = new Blob([body]);
-        const fileName = `${filename}.${extension}`;
-
-        const link = document.createElement("a");
-        // Browsers that support HTML5 download attribute
-        if (link.download !== undefined) {
-            const url = URL.createObjectURL(blob);
-            link.setAttribute("href", url);
-            link.setAttribute("download", fileName);
-            link.style.visibility = "hidden";
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }
-    }
+    //function base64ToArrayBuffer(base64: string) {
+    //    const binaryString = window.atob(base64); // Comment this if not using base64
+    //    const bytes = new Uint8Array(binaryString.length);
+    //    return bytes.map((byte, i) => binaryString.charCodeAt(i));
+    //}
+//
+//    function createAndDownloadBlobFile(
+//        body: any,
+//        filename: any,
+//        extension = "pdf"
+//    ) {
+//        const blob = new Blob([body]);
+//        const fileName = `${filename}.${extension}`;
+//
+//        const link = document.createElement("a");
+//        // Browsers that support HTML5 download attribute
+//        if (link.download !== undefined) {
+//            const url = URL.createObjectURL(blob);
+//            link.setAttribute("href", url);
+//            link.setAttribute("download", fileName);
+//            link.style.visibility = "hidden";
+//            document.body.appendChild(link);
+//            link.click();
+//            document.body.removeChild(link);
+//        }
+//    }
 
     const savePart = () => {
         setHotPart({
